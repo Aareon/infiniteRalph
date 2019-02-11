40312ac0d435408fb18ecec747a3655c6d9048b4
Aareon Sullivan <askully13@gmail.com>
2019-02-11 15:43:44 -0800

    Clean up water

--- water/__init__.py
+++ water/__init__.py
@@ -2,32 +2,41 @@ from panda3d.core import *
 
 
 class WaterNode(NodePath):
-
     def __init__(self, x1, y1, x2, y2, z):
-        NodePath.__init__(self,"Water")
+        NodePath.__init__(self, "Water")
         self.reparentTo(render)
-        print('setting up water plane at z=' + str(z))
+        print(f"setting up water plane at z={z}")
 
         # Water surface
-        maker = CardMaker('water')
+        maker = CardMaker("water")
         maker.setFrame(x1, x2, y1, y2)
 
         self.waterNP = self.attachNewNode(maker.generate())
         self.waterNP.setHpr(0, -90, 0)
         self.waterNP.setPos(0, 0, z)
         self.waterNP.setTransparency(TransparencyAttrib.MAlpha)
-        self.waterNP.setShader(loader.loadShader('water/water.sha'))
-        self.waterNP.setShaderInput('wateranim', Vec4(0.03, -0.015, 64.0, 0)) # vx, vy, scale, skip
+        self.waterNP.setShader(loader.loadShader("water/water.sha"))
+        self.waterNP.setShaderInput(
+            "wateranim",
+            Vec4(0.03, -0.015, 64.0, 0)  # vx, vy, scale, skip
+        )
+
         # offset, strength, refraction factor (0=perfect mirror, 1=total refraction), refractivity
-        self.waterNP.setShaderInput('waterdistort', Vec4(0.4, 4.0, 0.35, 0.045))
+        self.waterNP.setShaderInput(
+            "waterdistort",
+            Vec4(0.4, 4.0, 0.35, 0.045)
+        )
 
         # Reflection plane
-        self.waterPlane = Plane(Vec3(0, 0, z + 1), Point3(0, 0, z))
-        planeNode = PlaneNode('self.waterPlane')
+        self.waterPlane = Plane(
+            Vec3(0, 0, z + 1),
+            Point3(0, 0, z)
+        )
+        planeNode = PlaneNode("self.waterPlane")
         planeNode.setPlane(self.waterPlane)
 
         # Buffer and reflection camera
-        buffer = base.win.makeTextureBuffer('waterBuffer', 512, 512)
+        buffer = base.win.makeTextureBuffer("waterBuffer", 512, 512)
         buffer.setClearColor(Vec4(0, 0, 0, 1))
 
         cfa = CullFaceAttrib.makeReverse()
@@ -42,7 +51,7 @@ class WaterNode(NodePath):
         cam = self.watercamNP.node()
         cam.setLens(base.camLens)
         cam.setInitialState(rs)
-        cam.setTagStateKey('Clipped')
+        cam.setTagStateKey("Clipped")
         #cam.setTagState('True', RenderState.make(sa))
 
         # ---- water textures ---------------------------------------------
@@ -51,12 +60,12 @@ class WaterNode(NodePath):
         tex0 = buffer.getTexture()
         tex0.setWrapU(Texture.WMClamp)
         tex0.setWrapV(Texture.WMClamp)
-        ts0 = TextureStage('reflection')
+        ts0 = TextureStage("reflection")
         self.waterNP.setTexture(ts0, tex0)
 
         # distortion texture
-        tex1 = loader.loadTexture('water/water.png')
-        ts1 = TextureStage('distortion')
+        tex1 = loader.loadTexture("water/water.png")
+        ts1 = TextureStage("distortion")
         self.waterNP.setTexture(ts1, tex1)
 
         # ---- Fog --- broken
